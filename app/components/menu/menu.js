ab3f57d0c92ccc8e01ed40587e1717718ba6b426
Alex Ramsdell <alexramsdell@gmail.com>
2016-08-11 17:05:23 -0700

    Rename sidemenu component to menu

--- app/components/menu/menu.js
+++ app/components/menu/menu.js
@@ -5,12 +5,12 @@ steal(
 
         return can.Component.extend({ 
 
-            tag: 'task-sidemenu',
-            template: can.view('/app/components/sidemenu/sidemenu.stache'),
+            tag: 'task-menu',
+            template: can.view('/app/components/menu/menu.stache'),
             viewModel: null,
             events: {
                 'inserted': function(el, ev) {
-                    console.log('sidemenu inserted');
+                    console.log('menu inserted');
                 },
             }
 
