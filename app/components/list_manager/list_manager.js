28c418a746a7c179454913dbeb3aab5cb5fda037
Alex Ramsdell <alexramsdell@gmail.com>
2016-09-05 12:29:25 -0700

    task list title is an input in all cases now

--- app/components/list_manager/list_manager.js
+++ app/components/list_manager/list_manager.js
@@ -19,7 +19,6 @@ steal(
             tag: 'app-list-manager',
             template: ListManagerView, 
             viewModel: {
-                overflow: 'true',
                 taskLists: new can.List([ 
                     new TaskListModel({ 
                         title: 'Camping Trip',
