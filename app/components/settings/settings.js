f058c82aac682af839678ce4bc11716fc8e388f1
Alex Ramsdell <alexramsdell@gmail.com>
2016-09-09 12:57:00 -0700

    removed unused dependencies. added a stub jsonToMd function, not using a library because it is overkill for my purposes

--- app/components/settings/settings.js
+++ app/components/settings/settings.js
@@ -2,28 +2,38 @@ steal(
 
     'can',
 
+    'app/plugins/jsonToMd.js',
+
     './settings.stache!',
     './settings.less!',
 
     function(
         can, 
+        jsonToMd,
         settingsView
     ) {
 
         can.Component.extend({
+
             tag: 'app-settings',
             template: settingsView,
             viewModel: {
+
+                exportModalActive: false, 
                 exportFormat: 'markdown', 
+                exportContent: '',
                 cloudProvider: 'Google Drive', 
                 toggleSettings: function() {
                     this.attr('settingsActive', !this.attr('settingsActive'));
+                },
+                exportTaskLists: function() {
+                    var exportContentRaw = can.store.get('taskLists');
+                    jsonToMd(exportContentRaw);
+                    this.attr('exportContent', exportContentRaw);
+                    this.attr('exportModalActive', !this.attr('exportModalActive'));
                 }
             },
             events: {
-                'inserted': function() {
-                    console.log('inserted settings');
-                }
             } 
         });
 
---
+++ app/plugins/jsonToMd.js
@@ -0,0 +1,8 @@
+steal(function() {
+
+    return function(json) {
+        var parsedObj = JSON.parse(json);
+        console.log(parsed);
+    };
+
+});
