24e12405b93b0b63cc43d576aedff5f7dac92b46
Alex Ramsdell <alexramsdell@gmail.com>
2016-09-19 13:48:59 -0700

    improve storage.clear to include init()

--- app/plugins/storage/storage.js
+++ app/plugins/storage/storage.js
@@ -12,59 +12,65 @@ steal(
 
     ) {
 
-    return {
+    var Storage = function() {
 
-        init: function() {
+        return {
 
-            if (!store.get('tasklist')) {
-                console.log('initializing tasklist localstorage for first time');
-                store.set('tasklist', {
-                    'taskLists': [ 
-                        new TaskList({}).serialize() 
-                    ],
-                    'settings': {
-                        theme: themes[0],
+            init: function() {
+
+                if (!store.get('tasklist')) {
+                    console.log('initializing tasklist localstorage for first time');
+                    store.set('tasklist', {
+                        'taskLists': [ 
+                            new TaskList({}).serialize() 
+                        ],
+                        'settings': {
+                            theme: themes[0],
+                        }
+                    });
+                } else {
+                    console.log('using existing tasklist localstorage data');
+                } 
+            },
+
+            clear: function() {
+                store.set('tasklist','');
+                this.init();
+            },
+
+            get: function(path) {
+
+                if (!path) {
+
+                    return store.get('tasklist');
+
+                } else {
+
+                    var parts = path.split('.');
+                    var target = store.get('tasklist');
+                    while (parts.length) {
+                        target = target[ parts.shift() ];
                     }
-                });
-            } else {
-                console.log('using existing tasklist localstorage data');
-            } 
-        },
+                    return target;
 
-        clear: function() {
-            store.set('tasklist','');
-        },
+                }
+            },
 
-        get: function(path) {
-
-            if (!path) {
-
-                return store.get('tasklist');
-
-            } else {
+            set: function(path, data) {
 
                 var parts = path.split('.');
-                var target = store.get('tasklist');
-                while (parts.length) {
+                var appData = store.get('tasklist');
+                var target = appData;
+                data = (data.serialize) ? data.serialize() : data;
+
+                while (parts.length > 1) {
                     target = target[ parts.shift() ];
                 }
-                return target;
-
+                target[parts[0]] = data;
+                store.set('tasklist', appData);
             }
-        },
-
-        set: function(path, data) {
-
-            var parts = path.split('.');
-            var appData = store.get('tasklist');
-            var target = appData;
-            data = (data.serialize) ? data.serialize() : data;
-
-            while (parts.length > 1) {
-                target = target[ parts.shift() ];
-            }
-            target[parts[0]] = data;
-            store.set('tasklist', appData);
-        }
+        };
     };
+
+    return new Storage();
 });
